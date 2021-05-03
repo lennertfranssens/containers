6e86535c1c56c6b2f1c388317dd7dfc719833da8
Lennert Franssens <lennert.franssens@gmail.com>
2021-05-03 21:32:05 +0200

    Update run_ubuntu_container.sh

--- run_ubuntu_container.sh
+++ run_ubuntu_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --name=ubuntu_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/catkin_ws:/home/ros/catkin_ws ubuntu_container
+docker run --name=ubuntu_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/ubuntu_container_home:/home/user ubuntu_container
 # stop container
 #docker stop ubuntu_container
 # remove container
