40e1117c37a5c1f8257671e91f697dabb298fa18
Lennert Franssens <lennert.franssens@gmail.com>
2021-05-03 21:55:05 +0200

    Update run_ubuntu_container.sh

--- run_ubuntu_container.sh
+++ run_ubuntu_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This permits the root user on the local machine to connect to X windows display
+xhost +local:docker
+
 docker run --name=ubuntu_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/ubuntu_container_home:/home/user ubuntu_container
 # stop container
 #docker stop ubuntu_container
