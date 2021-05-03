233da81a9f979be542a8579f37660db35b3f3ade
Lennert Franssens <lennert.franssens@gmail.com>
2021-05-03 21:54:56 +0200

    Update run_fedora_container.sh

--- run_fedora_container.sh
+++ run_fedora_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This permits the root user on the local machine to connect to X windows display
+xhost +local:docker
+
 docker run --name=fedora_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/fedora_container_home:/home/user fedora_container
 # stop container
 #docker stop fedora_container
