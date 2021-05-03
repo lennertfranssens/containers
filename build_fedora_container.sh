a6957a9cb00fb99c90e04a914baa38812a734cb0
Lennert Franssens <lennert.franssens@gmail.com>
2021-05-03 21:28:52 +0200

    Update build_fedora_container.sh

--- build_fedora_container.sh
+++ build_fedora_container.sh
@@ -6,7 +6,7 @@ read PASSWORD
 docker rm fedora_container
 docker rmi fedora_container
 docker build --build-arg PASSWORD=$PASSWORD -t fedora_container .
-docker run --name=fedora_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/fedora_container_ws:/home/user fedora_container
+docker run --name=fedora_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/fedora_container_home:/home/user fedora_container
 # stop container
 #docker stop fedora_container
 # remove container
