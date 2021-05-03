4e7c8ae64940e055f4abbc1b39dc0e9bb3186f6d
Lennert Franssens <lennert.franssens@gmail.com>
2021-05-03 20:59:15 +0200

    Only ask password to set and use shared volume

--- build_fedora_container.sh
+++ build_fedora_container.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-printf 'Git email: '
-read GIT_EMAIL
-printf 'Git username: '
-read GIT_USERNAME
-printf 'Git password: '
-read GIT_PASSWORD
-
 docker rm fedora_container
 docker rmi fedora_container
-docker build --build-arg GIT_EMAIL=$GIT_EMAIL --build-arg GIT_USERNAME=$GIT_USERNAME --build-arg GIT_PASSWORD=$GIT_PASSWORD -t fedora_container .
-docker run --name=fedora_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix fedora_container
+docker build --build-arg PASSWORD=$PASSWORD -t fedora_container .
+docker run --name=fedora_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/fedora_container_ws:/home/user fedora_container
 # stop container
 #docker stop fedora_container
 # remove container
