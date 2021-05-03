881097931dc8ea410ac72eb574e34b699ef75d33
Lennert Franssens <lennert.franssens@gmail.com>
2021-05-03 21:29:50 +0200

    Update run_fedora_container.sh

--- run_fedora_container.sh
+++ run_fedora_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --name=fedora_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix fedora_container
+docker run --name=fedora_container -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/fedora_container_home:/home/user fedora_container
 # stop container
 #docker stop fedora_container
 # remove container
